0625e983f182e8db7e8218746432491db678a3e2
tatazakup <tatazakup_tatazakup@hotmail.com>
2021-04-08 17:38:12 +0700

    hello world 2

--- routes/Oauth.js
+++ routes/Oauth.js
@@ -5,14 +5,43 @@ router.get('/register', function (req, res, next) {
     res.render('sign_up');
 });
 
-router.post('/register', function (req, res, next) {
-    console.log(req.body.username);
-
-    res.render('sign_up');
-});
-
 router.get('/login', function (req, res, next) {
     res.render('sign_in');
+});
+
+router.post('/register', function (req, res, next) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    console.log(req.body.ssn);
+    console.log(req.body.phone_number);
+
+    // หาก ลงทะเบียนสำเร็จ
+    if (true == true){
+        // จะเก็บ id object ลง username
+        res.cookie('username',req.body.username, { maxAge: 900000, httpOnly: true });
+        console.log('register successfully');
+    } else {
+        res.render('sign_up');
+    }
+
+    res.render('index');
+    
+});
+
+router.post('/login', function (req, res, next) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+
+    // หาก ล็อกอินสำเร็จ
+    if (true == true){
+        // จะเก็บ id object ลง username
+        res.cookie('username',req.body.username, { maxAge: 900000, httpOnly: true });
+        console.log('login successfully');
+    } else {
+        res.render('sign_in');
+    }
+
+    res.render('index');
 });
 
 module.exports = router;
